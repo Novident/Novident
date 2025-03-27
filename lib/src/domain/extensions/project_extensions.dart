3192d72e7a70fec524e2d210299636ec7b05864b
CatHood0 <santiagowmar@gmail.com>
2025-03-27 03:20:19 -0400

    Chore: added method for get template sheet folder

--- lib/src/domain/extensions/project_extensions.dart
+++ lib/src/domain/extensions/project_extensions.dart
@@ -1,18 +1,19 @@
 import 'package:novident_remake/src/domain/entities/node/node.dart';
+import 'package:novident_remake/src/domain/entities/project/project.dart';
 import 'package:novident_remake/src/domain/entities/tree_node/document_resource.dart';
 import 'package:novident_remake/src/domain/entities/tree_node/folder.dart';
-import 'package:novident_remake/src/domain/entities/tree_node/root_node.dart';
 import 'package:novident_remake/src/domain/enums/enums.dart';
 import 'package:novident_remake/src/domain/extensions/cast_extension.dart';
-import 'package:novident_remake/src/domain/extensions/object_extension.dart';
 
-extension ResourceExtension on Root {
+extension ResourceExtension on Project {
   List<DocumentResource> getResources({bool onlyUseResearch = true}) {
     final List<DocumentResource> resources = <DocumentResource>[];
     if (onlyUseResearch) {
-      final Folder? research = visitAllNodes(
-        shouldGetNode: (node) => node is Folder && node.type.isResearchFolder,
-      ).cast<Folder?>();
+      final Folder? research = root
+          .visitAllNodes(
+            shouldGetNode: (node) => node is Folder && node.type.isResearchFolder,
+          )
+          .cast<Folder?>();
       if (research != null) {
         return resources
           ..addAll(research
@@ -21,27 +22,51 @@ extension ResourceExtension on Root {
       }
     }
     return resources
-      ..addAll(whereDeep(
-        (Node node) => node is DocumentResource && node.isResource,
-      ).cast<DocumentResource>());
+      ..addAll(root
+          .whereDeep(
+            (Node node) => node is DocumentResource && node.isResource,
+          )
+          .cast<DocumentResource>());
+  }
+
+  /// Return TemplatesSheet folder
+  Folder? getTemplateSheet() {
+    final Folder? template = !config.cache.templatePosition.value.isNegative
+        ? root.elementAt(config.cache.templatePosition.value).cast<Folder>()
+        : root
+            .visitNode(
+              shouldGetNode: (Node node) =>
+                  node is Folder && node.type.isTemplatesSheetFolder,
+              // commonly, templates sheet is
+              // near to the end of the project
+              reversed: true,
+            )
+            .cast<Folder?>();
+    if (template != null) {
+      if (config.cache.templatePosition.value.isNegative) {
+        config.cache.templatePosition.value = root.indexOf(
+          template,
+          root.length - 1,
+        );
+      }
+      return template;
+    }
+    return null;
   }
 
   /// Return all the nodes into the TemplatesSheet folder
   ///
-  /// - [templatePosition]: is the index where we can found the template
-  ///
-  /// _usually, all the projects tracks the position of the template folder_
-  /// _to get it fastly without traversing the entire project (even if it is being moved/updated)_
-  List<Node> getTemplates({int? templatePosition}) {
-    final Folder? template = !templatePosition.isNull
-        ? elementAt(templatePosition!).cast<Folder>()
-        : visitNode(
-            shouldGetNode: (node) =>
-                node is Folder && node.type.isTemplatesSheetFolder,
-            // commonly, templates sheet is
-            // near to the end of the project
-            reversed: true,
-          ).cast<Folder?>();
+  List<Node> getTemplates() {
+    final Folder? template = !config.cache.templatePosition.value.isNegative
+        ? root.elementAt(config.cache.templatePosition.value).cast<Folder>()
+        : root
+            .visitNode(
+              shouldGetNode: (node) => node is Folder && node.type.isTemplatesSheetFolder,
+              // commonly, templates sheet is
+              // near to the end of the project
+              reversed: true,
+            )
+            .cast<Folder?>();
     if (template != null) {
       return <Node>[...template.children];
     }
